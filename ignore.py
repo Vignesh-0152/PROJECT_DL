585fcbad5d762ba4306d3bb22710686986c47f4f
Vignesh-0152 <sgvignesh2005@gmail.com>
2025-07-05 22:20:36 +0530

    some bugs have been fixed

--- ignore.py
+++ ignore.py
@@ -1,7 +1,7 @@
 from preprocess import imgprocess
-
-dataset = imgprocess(
-        r"C:\Users\svign_ggx9gjx\Desktop\soft_computing\VIGNESH\VIGNESH\yolov8-multiple-vehicle-detection-main\yolov8-multiple-vehicle-detection\supervised_accidents_only\test\images", 
-        r"c:\Users\svign_ggx9gjx\Desktop\soft_computing\VIGNESH\VIGNESH\yolov8-multiple-vehicle-detection-main\yolov8-multiple-vehicle-detection\supervised_accidents_only\test\labels"
+import tensorflow as tf
+import numpy as np
+datasets = imgprocess(
+        r"C:\Users\svign_ggx9gjx\Desktop\soft_computing\VIGNESH\VIGNESH\yolov8-multiple-vehicle-detection-main\yolov8-multiple-vehicle-detection\dataset-vehicles\valid\images", 
+        r"C:\Users\svign_ggx9gjx\Desktop\soft_computing\VIGNESH\VIGNESH\yolov8-multiple-vehicle-detection-main\yolov8-multiple-vehicle-detection\dataset-vehicles\valid\labels"
     )
-print((dataset))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+print(tf.shape(datasets.label[2]))
+# tensor = datasets.label[2].numpy()
+# np.set_printoptions(threshold=np.inf, linewidth=np.inf, suppress=True)
+
+# # Now print
+# print(tensor[5,...])
+# print(datasets.cls)
