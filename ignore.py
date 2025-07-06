3f7c01e8d3e625661d0fc37932d2d440a1c3d4c4
Vignesh-0152 <sgvignesh2005@gmail.com>
2025-07-06 08:56:50 +0530

    commit for minor changes

--- ignore.py
+++ ignore.py
@@ -5,3 +5,9 @@ datasets = imgprocess(
         r"C:\Users\svign_ggx9gjx\Desktop\soft_computing\VIGNESH\VIGNESH\yolov8-multiple-vehicle-detection-main\yolov8-multiple-vehicle-detection\dataset-vehicles\valid\images", 
         r"C:\Users\svign_ggx9gjx\Desktop\soft_computing\VIGNESH\VIGNESH\yolov8-multiple-vehicle-detection-main\yolov8-multiple-vehicle-detection\dataset-vehicles\valid\labels"
     )
+print(tf.reduce_sum(datasets.label[0]))
+print(tf.reduce_sum(datasets.label[1]))
+print(tf.reduce_sum(datasets.label[2]))
+print(tf.math.count_nonzero(datasets.label[0]))
+print(tf.math.count_nonzero(datasets.label[1]))
+print(tf.math.count_nonzero(datasets.label[2]))
