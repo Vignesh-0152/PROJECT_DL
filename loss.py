c982016544891d1109843ba2192ea509b912731b
Vignesh-0152 <sgvignesh2005@gmail.com>
2025-07-13 08:35:44 +0530

    commit for minor changes"

--- loss.py
+++ loss.py
@@ -1,8 +1,17 @@
-from loss import bbloss, classloss, objloss
-from tensorflow.keras.layers import Input
-y_pred = y_true = Input(shape=(8400,8), batch_size= 8)
+import tensorflow as tf
+from loss import bbloss, classloss, objloss, CustomLoss
+
+# Simulate fake ground truth and prediction
+y_true = tf.random.uniform((1, 8400, 8))
+y_pred = tf.random.uniform((1, 8400, 8))
+
+# Call the custom loss functions
 x = bbloss()(y_true, y_pred)
 y = classloss()(y_true, y_pred)
 z = objloss()(y_true, y_pred)
-
-print(x.shape)
+a = CustomLoss()(y_true, y_pred)
+# Print actual scalar loss values
+print("BBox Loss:", x.shape, type(y), y.dtype)
+print("Class Loss:", y.shape, type(y), y.dtype)
+print("Obj Loss:", z.shape, type(z), y.dtype)
+print("custom Loss:", a.shape, type(a), a.dtype)
